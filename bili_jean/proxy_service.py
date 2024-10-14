19bffdabb381a39d61d7c55c90c0c3d8c8718a27
Chaojie Yan <ushareroses@gmail.com>
2024-10-14 22:22:46 +0800

    feat: proxy of video stream

--- bili_jean/proxy_service.py
+++ bili_jean/proxy_service.py
@@ -7,7 +7,7 @@ from typing import Dict, Optional
 import requests
 from requests import Response
 
-from .models import GetVideoInfoResponse
+from .models import GetVideoInfoResponse, GetVideoStreamResponse
 
 
 HEADERS = {
@@ -26,6 +26,7 @@ HEADERS = {
 TIMEOUT = 5
 
 SRC_VIDEO_INFO_URL = 'https://api.bilibili.com/x/web-interface/view'
+SRC_VIDEO_STREAM_URL = 'https://api.bilibili.com/x/player/wbi/playurl'
 
 
 class ProxyService:
@@ -58,3 +59,98 @@ class ProxyService:
         response = cls._get_video_info_response(bvid, aid)
         data = json.loads(response.content.decode('utf-8'))
         return GetVideoInfoResponse.model_validate(data)
+
+    @classmethod
+    def _get_video_stream_response(
+        cls,
+        cid: int,
+        bvid: Optional[str] = None,
+        aid: Optional[int] = None,
+        qn: Optional[int] = None,
+        fnval: int = 16,
+        fourk: int = 1,
+    ) -> Response:
+        if all([id_val is None for id_val in (bvid, aid)]):
+            raise ValueError("At least one of bvid and aid is necessary")
+
+        params: Dict = {}
+        if bvid is not None:
+            params.update({'bvid': bvid})
+        else:
+            params.update({'avid': aid})
+        params.update({'cid': cid})
+
+        if qn is not None:
+            params.update({'qn': qn})
+
+        params.update({
+            'fnval': fnval,
+            'fourk': fourk
+        })
+
+        response: Response = requests.get(
+            SRC_VIDEO_STREAM_URL,
+            params=params,
+            headers=HEADERS,
+            timeout=TIMEOUT,
+        )
+        return response
+
+    @classmethod
+    def get_video_stream(
+        cls,
+        cid: int,
+        bvid: Optional[str] = None,
+        aid: Optional[int] = None,
+        qn: Optional[int] = None,
+        fnval: int = 1,
+        fourk: int = 1,
+    ) -> GetVideoStreamResponse:
+        """
+        get video stream's info which is with /video/ namespace
+        :param cid: Identifier of codec
+        :type cid: int
+        :param bvid: BV ID of video
+        :type bvid: Optional[str]
+        :param aid: AV ID of video
+        :type bvid: Optional[int]
+        :param qn: video's quality, which is noneffective for DASH format
+                   | 6    | 240P    |                                                  |
+                   | 16   | 360P    |                                                  |
+                   | 32   | 480P    |                                                  |
+                   | 64   | 720P    |                                                  |
+                   | 74   | 720P60  |                                                  |
+                   | 80   | 1080P   | login needed                                     |
+                   | 112  | 1080P+  | VIP needed                                       |
+                   | 116  | 1080P60 | VIP needed                                       |
+                   | 120  | 4K      | VIP needed, `fnval&128=128` and `fourk=1`        |
+                   | 125  | HDR     | VIP needed, only support DASH, `fnval&64=64`     |
+                   | 126  | Dolby   | VIP needed, only support DASH, `fnval&512=512`   |
+                   | 127  | 8K      | VIP needed, only support DASH, `fnval&1024=1024` |
+        :type qn: Optional[int]
+        :param fnval: binary which stands for attributions
+                      | 1    | MP4                 | exclusive with DASH                                 |
+                      | 16   | DASH                | exclusive with MP4                                  |
+                      | 64   | HDR or not          | DASH is necessary, VIP needed, only H.265, `qn=125` |
+                      | 128  | 4K or not           | VIP needed, `fourk=1` and `qn=120`                  |
+                      | 256  | Dolby sound or not  | DASH is necessary, VIP needed                       |
+                      | 512  | Dolby vision or not | DASH is necessary, VIP needed                       |
+                      | 1024 | 8K or not           | DASH is necessary, VIP needed, `qn=127`             |
+                      | 2048 | AV1 codec or not    | DASH is necessary                                   |
+                      support 'or' for combination,
+                      e.g. DASH format, and HDR, fnval = 16 | 64 = 80
+        :type fnval: int
+        :param fourk: 4K or not
+        :type fourk: int
+        :return: GetVideoStreamResponse
+        """
+        response = cls._get_video_stream_response(
+            cid,
+            bvid,
+            aid,
+            qn,
+            fnval,
+            fourk,
+        )
+        data = json.loads(response.content.decode('utf-8'))
+        return GetVideoStreamResponse.model_validate(data)
